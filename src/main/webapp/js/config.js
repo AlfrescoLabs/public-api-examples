0395c3395ca657dc08c34290dd3c016fa0052dc7
Gethin James <Gethin.James@alfresco.com>
2012-10-22 10:24:45 +0100

    Added a simple html only example for the OAuth dance

---
+++ src/main/webapp/js/config.js
@@ -0,0 +1,9 @@
+var config = {
+  client_id:"l7xx141f984658a14c9389a33164c23abe5d",
+  client_secret:"c1475938b3ba45d5a40c2fca81e8479d",
+  redirect_uri:"http://localhost:8090/alfapi/example_htmlonly/mycallback.html",
+  authURL:"https://api.alfresco.com/auth/oauth/versions/2/authorize",
+  tokenURL:"https://api.alfresco.com/auth/oauth/versions/2/token",
+  scope:"public_api",
+  aTestUrl:"https://api.alfresco.com/alfresco.com/public/alfresco/versions/1/sites/public-api-trial-site"
+};
