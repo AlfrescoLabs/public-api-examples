a001e0241aab2dffd74d07775f07bdb607bf5657
Gethin James <Gethin.James@alfresco.com>
2012-10-23 08:43:41 +0100

    Revert "Revert "Clearer config comments""
    This reverts commit a6cd9a644e6f64c108b248ff5ab349e0008820b2.

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -1,3 +1,10 @@
+/* Use the values from your account on the Alfresco Developer Portal (developer.alfresco.com)
+ * 
+ * Set client_id to the value of "Api Key" (on the "Auth" tab when you edit an application in the portal)
+ * Set client_secret to the value of "Key Secret" (on the "Auth" tab when you edit an application in the portal)
+ * For the simple html example to work, the redirect_uri should be set to "http://localhost:8090/alfapi/example_htmlonly/mycallback.html"
+ * (The redirect_uri MUST match the value you set as the "Callback URL" on the "Auth" tab in the portal)
+ */
 var config = {
   client_id:"l7xx141f984658a14c9389a33164c23abe5d",
   client_secret:"c1475938b3ba45d5a40c2fca81e8479d",
